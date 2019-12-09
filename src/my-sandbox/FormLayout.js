510e7d47251ed8f852a2aae8c038a80c9661d635
thoughtdreams <rshekar@gmail.com>
2019-12-09 21:02:41 +0530

    Adding scrollable content to form

--- src/my-sandbox/FormLayout.js
+++ src/my-sandbox/FormLayout.js
@@ -6,6 +6,7 @@ import {
   CheckBox,
   Form,
   FormField,
+  Layer,
   RadioButtonGroup,
   Select,
   Heading
@@ -14,42 +15,56 @@ import SandboxComponent from "../SandboxComponent";
 
 export default () => (
   <SandboxComponent pad="none">
-    <Grid
-      areas={[["header"], ["content"], ["footer"]]}
-      columns={["flex"]}
-      rows={["xsmall", "auto", "xxsmall"]}
-      gap="small"
-    >
-      <Box gridArea="header" background="neutral-3" pad="large">
-        <Heading>My Form</Heading>
-      </Box>
-      <Box gridArea="content" background="light-2" pad="large">
-        <Form onSubmit={({ value }) => console.log("Submit: ", value)}>
-          <FormField name="name" label="Name" required={true} />
-          <FormField
-            label="Continent"
-            name="select"
-            component={Select}
-            options={["North America", "South America", "Asia"]}
-          />
-          <FormField name="address" label="Address" />
-          <FormField
-            name="acknowledge"
-            component={CheckBox}
-            pad={true}
-            label="acknowledge"
-          />
-          <FormField
-            name="day"
-            component={RadioButtonGroup}
-            pad={true}
-            options={["Tuesday", "Friday"]}
-          />
-        </Form>
-      </Box>
-      <Box gridArea="footer" background="white">
-        <Button type="submit" label="Submit" primary={true} stretch={false} />
-      </Box>
-    </Grid>
+    <Box fill={true} align="center" justify="center">
+      <Layer position="center" modal={true}>
+        <Grid
+          areas={[["header"], ["content"], ["footer"]]}
+          columns={["medium"]}
+          rows={["xsmall", "medium", "xxsmall"]}
+          gap="small"
+        >
+          <Box gridArea="header" background="neutral-3" pad="large">
+            <Heading>My Form</Heading>
+          </Box>
+          <Box
+            gridArea="content"
+            background="light-2"
+            pad="large"
+            overflow="scroll"
+          >
+            <Form onSubmit={({ value }) => console.log("Submit: ", value)}>
+              <FormField name="name" label="Name" required={true} />
+              <FormField
+                label="Continent"
+                name="select"
+                component={Select}
+                options={["North America", "South America", "Asia"]}
+              />
+              <FormField name="address" label="Address" />
+              <FormField
+                name="acknowledge"
+                component={CheckBox}
+                pad={true}
+                label="acknowledge"
+              />
+              <FormField
+                name="day"
+                component={RadioButtonGroup}
+                pad={true}
+                options={["Tuesday", "Friday"]}
+              />
+            </Form>
+          </Box>
+          <Box gridArea="footer" background="white">
+            <Button
+              type="submit"
+              label="Submit"
+              primary={true}
+              stretch={false}
+            />
+          </Box>
+        </Grid>
+      </Layer>
+    </Box>
   </SandboxComponent>
 );
